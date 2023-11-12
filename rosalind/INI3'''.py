3fd3713d00058397cf57d6ae1cc82acf4b7f6ade
2502200 <148723889+2502200@users.noreply.github.com>
2023-11-12 22:00:27 +0100

    Added INI3 for Rosalind

---
+++ rosalind/INI3'''.py
@@ -0,0 +1,4 @@
+str = "cotYFwBpVbLAIpG9Xj6FPelodytesrGX2zTuyKYoKidAyIQkfkvzW8s2uDx2lHiVVUPam3ZucVyhmZP53kM2azAcuICKFrHh2q40brwDA7Y0mQRmnhcyBalQrsaYjJMDzjzCCtA5QlRBoUSmf8V9bgtqYT4QrosmarusOqB7PFPuzX4ury0GKWMESe8SOOGA."
+ab = str[20:29] 
+cd = str[156:164]
+print(ab, cd)
