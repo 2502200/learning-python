26aa3728f571f2d394bdf75a8a2d517b10a08f05
2502200 <148723889+2502200@users.noreply.github.com>
2023-11-24 10:22:29 +0100

    Did point mutation counting as practice

---
+++ rosalind/counting_point_mutations.py
@@ -0,0 +1,21 @@
+#s = "GAGCCTACTAACGGGAT"
+#t = "CATCGTAATGACGGCCT"
+#to count point mutations defined by hamming distance (dh)
+
+
+
+#defina a compare function for the two dna sequences, s and t
+def compare(s, t):
+    dh = 0
+    s = "GAGCCTACTAACGGGAT"
+    t = "CATCGTAATGACGGCCT"
+#if any letter in a seq s doesn't equal to any letter in a seq t:
+#add 1 to the dh variable
+    for letter in range(len(s)):
+        if s[letter] != t[letter]:
+            dh += 1            
+    return(dh)
+
+compare(s, t) 
+
+
