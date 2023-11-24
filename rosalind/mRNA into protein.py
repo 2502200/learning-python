7a3e545e5a29a146b8137324f2980d8fe039ffc9
2502200 <148723889+2502200@users.noreply.github.com>
2023-11-24 10:11:11 +0100

    Added PROT exercise

---
+++ rosalind/mRNA into protein.py
@@ -0,0 +1,38 @@
+mRNA = 'AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA'
+RNAcodons= {
+    'UUU': 'F',     'CUU': 'L',     'AUU': 'I',     'GUU': 'V',
+    'UUC': 'F',     'CUC': 'L',     'AUC': 'I',     'GUC': 'V',
+    'UUA': 'L',     'CUA': 'L',     'AUA': 'I',     'GUA': 'V',
+    'UUG': 'L',     'CUG': 'L',     'AUG': 'M',     'GUG': 'V',
+    'UCU': 'S',     'CCU': 'P',     'ACU': 'T',     'GCU': 'A',
+    'UCC': 'S',     'CCC': 'P',     'ACC': 'T',     'GCC': 'A',
+    'UCA': 'S',     'CCA': 'P',     'ACA': 'T',     'GCA': 'A',
+    'UCG': 'S',     'CCG': 'P',     'ACG': 'T',     'GCG': 'A',
+    'UAU': 'Y',     'CAU': 'H',     'AAU': 'N',     'GAU': 'D',
+    'UAC': 'Y',     'CAC': 'H',     'AAC': 'N',     'GAC': 'D',
+    'UAA': 'Stop',  'CAA': 'Q',     'AAA': 'K',     'GAA': 'E',
+    'UAG': 'Stop',  'CAG': 'Q',     'AAG': 'K',     'GAG': 'E',
+    'UGU': 'C',     'CGU': 'R',     'AGU': 'S',     'GGU': 'G',
+    'UGC': 'C',     'CGC': 'R',     'AGC': 'S',     'GGC': 'G',
+    'UGA': 'Stop',  'CGA': 'R',     'AGA': 'R',     'GGA': 'G',
+    'UGG': 'W',     'CGG': 'R',     'AGG': 'R',     'GGG': 'G'
+}
+#def a translation function for mRNA
+def translation(mRNA):
+    proteinSEQ = ''
+#for every item from 0(beginning) to the entire length of mRNA seq, in steps of three
+    for item in range(0, len(mRNA), 3):
+#'letter' as a variable for RNAcodons of the mRNA seq again in steps of three
+        letter = RNAcodons[mRNA[item:item+3]]
+#if letter equals 'Stop', break the loop
+        if letter == 'Stop':
+            break
+        proteinSEQ += letter
+ 
+    return proteinSEQ
+
+translation(mRNA)
+
+
+
+
