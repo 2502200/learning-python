b42d0b46e853b47d30611f86b548ad3e1cb43f4d
2502200 <148723889+2502200@users.noreply.github.com>
2023-11-26 11:31:47 +0100

    Fixed a mistake or two in Mendels first law

--- rosalind/Mendels first law.py
+++ rosalind/Mendels first law.py
@@ -1,9 +1,10 @@
-k = "AA" 
-n = "Aa"  
-m = "aa"
+k = 2
+m = 2  
+n = 2
+#k is homozygous dominant (AA), m is heterozygous (Aa), n is homozygous recessive (aa)
 
 popul = k + m + n
-
-prob(Ax) = (k(k-1) + 2*k*m + 2*k*n + 0.75*(m-1) + 0.5*m*n + 0.5*m*n)/(popul*(popul-1))
+#the offspring can either be homozygous dominant or heterozygous (Ax)
+prob = (k*(k-1) + 2*k*m + 2*k*n + 0.75*(m-1) + 0.5*m*n + 0.5*m*n)/(popul*(popul-1))
 
 print(prob)
