586b179e3767c9886b6983ac8a231019fb342502
2502200 <148723889+2502200@users.noreply.github.com>
2023-11-26 11:34:32 +0100

    Added a comment in Mendels first law

--- rosalind/Mendels first law.py
+++ rosalind/Mendels first law.py
@@ -4,7 +4,7 @@ n = 2
 #k is homozygous dominant (AA), m is heterozygous (Aa), n is homozygous recessive (aa)
 
 popul = k + m + n
-#the offspring can either be homozygous dominant or heterozygous (Ax)
+#the offspring can either be homozygous dominant or heterozygous => (Ax); if any two organisms can mate
 prob = (k*(k-1) + 2*k*m + 2*k*n + 0.75*(m-1) + 0.5*m*n + 0.5*m*n)/(popul*(popul-1))
 
 print(prob)
